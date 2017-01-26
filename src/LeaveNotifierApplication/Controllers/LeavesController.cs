d50c220f7cb719535d2a3e9648befa7bbbec2fd3
adrian.onrails@gmail.com <adrian.onrails@gmail.com>
2017-01-26 17:49:31 +0800

    Test API LeavesController

---
+++ src/LeaveNotifierApplication/Controllers/LeavesController.cs
@@ -0,0 +1,34 @@
+﻿using LeaveNotifierApplication.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeaveNotifierApplication.Controllers
+{
+    [Route("api/[controller]")]
+    public class LeavesController : Controller
+    {
+        private ILeaveNotifierRepository _repo;
+
+        public LeavesController(ILeaveNotifierRepository repo)
+        {
+            _repo = repo;
+        }
+        [HttpGet]
+        public IActionResult Get()
+        {
+            try
+            {
+                return Ok(_repo.GetAllLeaves());
+            }
+            catch (Exception)
+            {
+            }
+            return BadRequest();
+        }
+    }
+}
